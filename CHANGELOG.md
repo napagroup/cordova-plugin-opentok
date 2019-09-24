17fbca08bd259fa940cdd417fa294a0a9bd4fab1
Philip J Browning II <philipjbrowning@gmail.com>
2019-09-24 14:59:08 -0400

    Upgrade to OpenTOk 2.16.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ## ChangeLog
 
+#### Version 3.4.9 (09.24.2019)
+- Update Android OpenTok SDK 2.16.2 -- [Sept 2019](https://tokbox.com/developer/sdks/android/release-notes.html)
+
 #### Version 3.4.8 (09.24.2019)
 - BugFix when disconnecting from a session.
 - triggerJSEvent should fire with or without supporting data.
--- src/android/OpenTokAndroidPlugin.java
+++ src/android/OpenTokAndroidPlugin.java
@@ -1017,7 +1017,7 @@ public class OpenTokAndroidPlugin extends CordovaPlugin
                     JSONObject payload = new JSONObject();
                     try {
                         payload.put("platform", "Android");
-                        payload.put("cp_version", "3.4.8");
+                        payload.put("cp_version", "3.4.9");
                     } catch (JSONException e) {
                         Log.i(TAG, "Error creating payload json object");
                     }
@@ -1026,7 +1026,7 @@ public class OpenTokAndroidPlugin extends CordovaPlugin
                     params.put("partner_id", apiKey);
                     params.put("payload", payload.toString());
                     params.put("source", "https://github.com/opentok/cordova-plugin-opentok");
-                    params.put("build", "2.16.1");
+                    params.put("build", "2.16.2");
                     params.put("session_id", sessionId);
                     if (connectionId != null) {
                         params.put("action", "cp_on_connect");
